1651eed61978524ee1011c5b1d89fc9ca647f0d2
Javierleal-debug <67740967+Javierleal-debug@users.noreply.github.com>
2020-09-15 12:57:34 -0600

    Update dia_2.py

--- dia_2.py
+++ dia_2.py
@@ -1,1 +1,15 @@
 dia_2.py
+
+import cv2
+
+captura=cv2.VideoCapture(0)
+
+while(captura.isOpened()):
+	ret,imagen=captura.read()
+	if ret==True:
+		cv2.imshow('video',imagen)
+		if cv2.waitKey(1) & 0xFF == ord('s'):
+			break
+
+captura.release()
+cv2.destroyAllWindows()
